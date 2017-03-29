13609b5cad509922c9581347fc49b954a00e1f7a
Soeren Schwert <s.schwert@avibit.com>
2017-03-29 13:31:18 +0200

    Simplify diff view

--- src/layer/Popup.js
+++ src/layer/Popup.js
@@ -227,10 +227,12 @@ L.Popup = L.DivOverlay.extend({
 		    marginBottom = parseInt(L.DomUtil.getStyle(this._container, 'marginBottom'), 10) || 0,
 		    containerHeight = this._container.offsetHeight + marginBottom,
 		    containerWidth = this._containerWidth,
-		    offset = new L.Point(this._containerLeft, -containerHeight - this._containerBottom);
+		    layerPos = new L.Point(this._containerLeft, -containerHeight - this._containerBottom);
 
-		var containerPos = L.DomUtil.getPosition(this._container)._add(this._map._getMapPanePos())._add(offset),
-		    padding   = L.point(this.options.autoPanPadding),
+		layerPos._add(L.DomUtil.getPosition(this._container));
+
+		var containerPos = layerPos._add(this._map._getMapPanePos()),
+		    padding = L.point(this.options.autoPanPadding),
 		    paddingTL = L.point(this.options.autoPanPaddingTopLeft || padding),
 		    paddingBR = L.point(this.options.autoPanPaddingBottomRight || padding),
 		    size = map.getSize(),
