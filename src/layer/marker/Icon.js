616b2b3b3af063ac040937a57bb9bb27dd9365ea
king_arthur23 <liuqing@liuqingtekiiMac.local>
2017-08-04 14:59:22 +0800

    Fix: icon transform origin wrong when options not given

--- src/layer/marker/Icon.js
+++ src/layer/marker/Icon.js
@@ -116,8 +116,8 @@ L.Icon = L.Class.extend({
 		if (anchor) {
 			img.style.marginLeft = (-anchor.x) + 'px';
 			img.style.marginTop  = (-anchor.y) + 'px';
+			img.style[L.DomUtil.TRANSFORM + "Origin"] = anchor.x + "px " + anchor.y + "px 0px";
 		}
-		img.style[L.DomUtil.TRANSFORM + "Origin"] = anchor ? (anchor.x + "px " + anchor.y + "px 0px") : "center center 0px";
 
 		if (size) {
 			img.style.width  = size.x + 'px';
